29d4c39f51d8096aeb3ffbda4fe21572e5481ed6
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-12-05 16:51:20 +0100

    Array di link in AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      link: [
+        {
+            text: "Link"
+        },
+        {
+            text: "Link"
+        },
+        {
+            text: "Link"
+        },
+        {
+            text: "Link"
+        },
+        {
+            text: "Link"
+        },
+        {
+            text: "Link"
+        },
+      ],
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped></style>
